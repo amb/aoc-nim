6e27673508f1cead6c84fd9e21d1781e3d6d8065
Tommi Hyppänen <thyppane@gmail.com>
2022-12-02 13:00:15 +0200

    Day 2 solve

--- 2/s.nim
+++ 2/s.nim
@@ -20,3 +20,20 @@ let lines = readFile("2/input").splitLines().filterIt(len(it) > 2)
 let games = lines.mapIt((it[0] - 'A', it[2] - 'X'))
 let score = games.mapIt(it.winLose + (it[1] + 1))
 echo sum(score)
+
+# Part 2
+
+# b + 1 - a = score/3
+# b = (a + score/3 - 1).mod3
+
+proc winLoseReverse(h: (int, int)): int =
+    return (h[0] + h[1] - 1).mod3
+
+# 0 = lose, 1 = draw, 2 = win 
+# 0 = rock, 1 = paper, 2 = scissors 
+doAssert [(0, 0), (0, 1), (0, 2)].mapIt(it.winLoseReverse) == @[2, 0, 1]
+doAssert [(1, 0), (1, 1), (1, 2)].mapIt(it.winLoseReverse) == @[0, 1, 2]
+doAssert [(2, 0), (2, 1), (2, 2)].mapIt(it.winLoseReverse) == @[1, 2, 0]
+
+let score2 = games.mapIt(it[1] * 3 + (it.winLoseReverse + 1))
+echo sum(score2)
