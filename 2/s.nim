899f83aa95838e320d42c409fedd3442fb3b4de2
Tommi Hyppänen <thyppane@gmail.com>
2022-12-02 11:14:19 +0200

    Solved 2

--- 2/s.nim
+++ 2/s.nim
@@ -1,5 +1,4 @@
 import strutils, sequtils, sugar, algorithm, math
 let sums = readFile("data/input").split("\n\n").
     map(i => sum(i.split().filterIt(len(it) > 0).mapIt(parseInt(it))))
-echo sorted(sums)[^1]
-
+echo sum(sorted(sums)[^3..^1])
