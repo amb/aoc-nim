8db23f262a2cbf05f043a7ac21327da1176dfd2d
Tommi Hyppänen <thyppane@gmail.com>
2022-12-02 12:40:11 +0200

    Solved 2.1

--- 1/s.nim
+++ 1/s.nim
@@ -1,5 +1,5 @@
 import strutils, sequtils, sugar, algorithm, math
-let sums = sorted(readFile("data/input").split("\n\n").
+let sums = sorted(readFile("1/input").split("\n\n").
     map(i => sum(i.split().filterIt(len(it) > 0).mapIt(parseInt(it)))))
 echo sums[^1]
 echo sum(sums[^3..^1])
--- 2/s.nim
+++ 2/s.nim
@@ -1,2 +1,22 @@
 import strutils, sequtils, sugar, algorithm, math
-echo "oops"
+
+proc `-`(a, b: char): int = ord(a) - ord(b)
+
+#  RPS <- player
+# R360
+# P036
+# S603
+
+# 0: lose, 3: draw, 6: win
+proc mod3(v: int): int = (v + 3) mod 3
+proc winLose(h: (int, int)): int =
+    return (h[1] + 1 - h[0]).mod3 * 3
+
+doAssert [(0, 0), (0, 1), (0, 2)].mapIt(it.winLose) == @[3, 6, 0]
+doAssert [(1, 0), (1, 1), (1, 2)].mapIt(it.winLose) == @[0, 3, 6]
+doAssert [(2, 0), (2, 1), (2, 2)].mapIt(it.winLose) == @[6, 0, 3]
+
+let lines = readFile("2/input").splitLines().filterIt(len(it) > 2)
+let games = lines.mapIt((it[0] - 'A', it[2] - 'X'))
+let score = games.mapIt(it.winLose + (it[1] + 1))
+echo sum(score)
