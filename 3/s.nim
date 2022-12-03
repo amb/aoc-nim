336a1221ea4dceb479127375ea55fe6152ee43a2
Tommi Hyppänen <thyppane@gmail.com>
2022-12-03 12:56:26 +0200

    Partitioning proc

--- 3/s.nim
+++ 3/s.nim
@@ -1,10 +1,17 @@
-import strutils, sequtils, sugar, algorithm, math, sets
+import strutils, strformat, sequtils, sugar, algorithm, math, sets
 import ../aoc
 
 # for i in 0 .. 25:
 #     echo chr(i+ord('a')) & " " & chr(i+ord('A'))
 # a->z = 1->25
 # A->Z = 27-52
+
+
+proc priority(c: char): int =
+    doAssert isAlphaAscii(c)
+    if isLowerAscii(c): c-'a'+1
+    elif isUpperAscii(c): c-'A'+27
+    else: 0
 
 iterator priorities(): int =
     for line in fil(3):
@@ -13,14 +20,20 @@ iterator priorities(): int =
         var si = intersection(toHashSet(line[0..<ll]), toHashSet(line[ll..<line.len]))
         if len(si) == 0:
             continue
-        let c = si.pop()
-        doAssert isAlphaAscii(c)
-        yield (if isLowerAscii(c): 
-                ord(c)-ord('a')+1
-            elif isUpperAscii(c): 
-                ord(c)-ord('A')+27
-            else: 0)
+        yield priority(si.pop())
 
-echo sum(toSeq(priorities))
+iterator prioritiesB(): int =
+    for group in fil(3).filterIt(len(it) > 0).partition(3):
+        if len(group) != 3:
+            # echo "no3"
+            continue
+        var si = intersection(toHashSet(group[0]), toHashSet(group[1]))
+        si = intersection(si, toHashSet(group[2]))
+        if len(si) == 0:
+            continue
+        doAssert len(si) == 1
+        yield priority(si.pop())
+
+echo sum(toSeq(prioritiesB))
 
 # echo sum()
--- aoc.nim
+++ aoc.nim
@@ -7,3 +7,18 @@ proc fil*(n: int): seq[string] =
 
 let lowerLetters* = toHashSet(toSeq(ord('a')..ord('z')))
 let upperLetters* = toHashSet(toSeq(ord('A')..ord('Z')))
+
+proc partition*[T](i: seq[T], ps: int): seq[seq[T]] =
+    doAssert len(i) mod ps == 0, fmt"Seq len {len(i)} not divisible with {ps}"
+    var os: seq[seq[T]] = newSeq[seq[T]]()
+    var c: seq[T]
+    var ci = 0
+    for v in i:
+        c.add(v)
+        ci += 1
+        if ci == ps:
+            ci = 0
+            os.add(c)
+            c.setLen(0)
+    return os
+
