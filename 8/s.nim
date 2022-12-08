b39193a2a0da7a5f8f8a80627bc851b574cdd4c3
Tommi Hyppänen <thyppane@gmail.com>
2022-12-08 21:29:55 +0200

    Horrible horrible horrible

--- 8/s.nim
+++ 8/s.nim
@@ -1,6 +1,11 @@
 include ../aoc
 
-let data = fil(8)
+type
+    TreeLocation = tuple[x: int, y: int]
+    TreeTable = Table[TreeLocation, int]
+    TreeItem = tuple[loc: TreeLocation, value: int]
+    WarpFunc = proc (ti: TreeItem): TreeItem
+    TreeScanFunc = proc (tr: TreeTable): TreeTable
 
 # let data = """
 # 30373
@@ -9,30 +14,23 @@ let data = fil(8)
 # 33549
 # 35390
 # """.splitLines
-
-type
-    TreeLocation = tuple[x: int, y: int]
-    SetOfTrees = Table[TreeLocation, int]
-    TreeItem = tuple[loc: TreeLocation, value: int]
-    WarpFunc = proc (ti: TreeItem): TreeItem
+let data = fil(8)
 
 # Account for newline at the end
-var height: Natural = data.len-1
-var width: Natural = data[0].len
+let height: Natural = data.len-1
+let width: Natural = data[0].len
 echo fmt"Grid size = x: {width}, y: {height}"
 doAssert width == height
 let asize = width
-let szm1 = asize-1
 
-# Gather data
-var trees: SetOfTrees
+var trees: TreeTable
 for y, l in data:
     if len(l) > 2:
         for x, c in l:
             doAssert x < asize and y < asize
             trees[(x, y)] = parseInt($c)
 
-proc printTrees(tr: SetOfTrees) =
+proc printTrees(tr: TreeTable) =
     var grid = newSeqWith(asize, newSeq[char](asize))
     echo "-".repeat(asize)
     for (k, v) in tr.pairs:
@@ -41,42 +39,72 @@ proc printTrees(tr: SetOfTrees) =
         echo i.join
     echo "-".repeat(asize)
 
-proc scanTrees(tr: SetOfTrees): SetOfTrees =
-    # Scan from top to bottom
-    var scanline: seq[TreeItem] = collect:
+proc scanVisible(tr: TreeTable): TreeTable =
+    var scanline = newSeq[int](asize)
+    var foundTrees: seq[TreeItem] = collect:
         for i in 0..<asize: (loc: (i, 0), value: tr[(i, 0)])
-    var foundTrees = scanline.deepCopy
     for x in 0..<asize:
         for y in 0..<asize:
-            if tr[(x, y)] > scanline[x].value:
-                let newEntry = (loc: (x: x, y: y), value: tr[(x, y)])
-                scanline[x] = newEntry
-                foundTrees.add(newEntry)
+            let gridVal = tr[(x, y)]
+            if gridVal > scanline[x]:
+                scanline[x] = gridVal
+                foundTrees.add((loc: (x: x, y: y), value: gridVal))
     return foundTrees.toTable
 
-proc tableWarp(tr: SetOfTrees, warp: WarpFunc): SetOfTrees =
+proc scanRoom(tr: TreeTable): TreeTable =
+    var lineMemory = newSeqWith(asize, newSeq[int](10))
+    var resTrees = tr
+    for y in 0..<asize:
+        # Top to bottom
+        for x in 0..<asize:
+            # Left to right
+            let gridVal = tr[(x, y)]
+            resTrees[(x, y)] = lineMemory[x][gridVal]
+            for g in 0..gridVal: lineMemory[x][g] = 0
+            for g in 0..9: inc lineMemory[x][g]
+
+    return resTrees
+
+proc tableWarp(tr: TreeTable, warp: WarpFunc): TreeTable =
     result = collect:
         for k, v in tr.pairs:
             let r = warp((k, v))
             {r[0]: r[1]}
 
-proc flipH(l: TreeLocation): TreeLocation = (l.x, szm1-l.y)
+proc flipH(l: TreeLocation): TreeLocation = (l.x, asize-1-l.y)
 proc tsp(l: TreeLocation): TreeLocation = (l.y, l.x)
 
-proc tfB(tr: SetOfTrees): SetOfTrees = tr.tableWarp(t => (t.loc.flipH, t.value))
-proc tfL(tr: SetOfTrees): SetOfTrees = tr.tableWarp(t => (t.loc.tsp, t.value))
-proc tfR(tr: SetOfTrees): SetOfTrees = tr.tableWarp(t => (t.loc.tsp.flipH, t.value))
+proc tfB(tr: TreeTable): TreeTable = tr.tableWarp(t => (t.loc.flipH, t.value))
+proc tfL(tr: TreeTable): TreeTable = tr.tableWarp(t => (t.loc.tsp, t.value))
+proc tfR(tr: TreeTable): TreeTable = tr.tableWarp(t => (t.loc.tsp.flipH, t.value))
 
 # Order matters
 doAssert trees.tfR.tfB.tfL == trees
 
-var scanned = newSeq[SetOfTrees]()
-scanned.add(scanTrees(trees))
-scanned.add(scanTrees(trees.tfB).tfB)
-scanned.add(scanTrees(trees.tfL).tfL)
-scanned.add(scanTrees(trees.tfR).tfB.tfL)
+proc allDir(tr: TreeTable, scanFunc: TreeScanFunc): seq[TreeTable] =
+    result.add(scanFunc(trees))
+    result.add(scanFunc(trees.tfB).tfB)
+    result.add(scanFunc(trees.tfL).tfL)
+    result.add(scanFunc(trees.tfR).tfB.tfL)
 
-echo scanned.mapIt(it.keys.toSeq).concat.deduplicate.len
+# echo trees.allDir(scanVisible).mapIt(it.keys.toSeq).concat.deduplicate.len
+let spaceScan = trees.allDir(scanRoom)
 
-# echo {(1,0): 'y', (0,5): 'x'}.type.name
-# echo {(1, 0): Natural(5)}.type.name
+var summed = newSeqWith(asize, newSeq[int](asize))
+var maxVal = ((0, 0), 0) 
+for x in 1..<asize-1:
+    for y in 1..<asize-1:
+        summed[y][x] = 1
+        for g in 0..<4:
+            summed[y][x] *= spaceScan[g][(x, y)]
+        let sval = summed[y][x]
+        if sval > maxVal[1]:
+            maxVal = ((x, y), sval)
+        
+echo maxVal
+
+# for i in summed:
+#     echo i.join(" ")
+
+# trees.tfB.printTrees
+# trees.printTrees
