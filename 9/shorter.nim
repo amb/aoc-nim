21a20436e4b037931b9df723bf1506c64b9f9ce6
Tommi Hyppänen <thyppane@gmail.com>
2022-12-09 17:08:07 +0200

    collect instead of mpas and filters

--- 9/shorter.nim
+++ 9/shorter.nim
@@ -6,9 +6,14 @@ proc follow(tail: var Vec2i, head: Vec2i) =
         tail += (if disp.x * disp.y == 0: disp/len(disp) else: disp/abs(disp))
 
 let mdir = {'L': vec2i(-1, 0), 'R': vec2i(1, 0), 'U': vec2i(0, -1), 'D': vec2i(0, 1)}.toTable
+let moves = collect:
+    for line in fil(9):
+        let (r, d, s) = line.scanTuple("$c $i")
+        if r: mdir[d] * s
+
 var knots = newSeq[Vec2i](10)
 var trail1, trail9: HashSet[(int, int)]
-for m in fil(9).mapIt(it.scanTuple("$c $i")).filterIt(it[0]).mapIt(mdir[it[1]] * it[2]):
+for m in moves:
     let dir = m/len(m)
     for step in 0..<m.len:
         knots[0] += dir
