a74bf2f1e99af97abbee031a327e864aecbef8a8
Tommi Hyppänen <thyppane@gmail.com>
2022-12-09 16:23:32 +0200

    20 lines

--- 9/shorter.nim
+++ 9/shorter.nim
@@ -1,31 +1,14 @@
 include ../aoc
 
-type
-    Vec2i = object
-        x, y: int
-
-proc vec(x, y: int): Vec2i = Vec2i(x: x, y: y)
-proc `*`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x*b, y: a.y*b)
-proc `/`(a: Vec2i, b: Vec2i): Vec2i = Vec2i(x: a.x div b.x, y: a.y div b.y)
-proc `/`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x div b, y: a.y div b)
-proc `-`(a, b: Vec2i): Vec2i = Vec2i(x: a.x - b.x, y: a.y - b.y)
-proc `+`(a, b: Vec2i): Vec2i = Vec2i(x: a.x + b.x, y: a.y + b.y)
-proc `+=`(a: var Vec2i, b: Vec2i) = a = a+b
-proc abs(a: Vec2i): Vec2i = Vec2i(x: abs(a.x), y: abs(a.y))
-proc len(a: Vec2i): int = max(abs(a.x), abs(a.y))
-proc asTuple(v: Vec2i): (int, int) = (v.x, v.y)
-
 proc follow(tail: var Vec2i, head: Vec2i) =
     let disp = head - tail
     if abs(disp.x) > 1 or abs(disp.y) > 1:
-        if disp.x * disp.y == 0: tail += disp/len(disp)
-        else: tail += disp/abs(disp)
+        tail += (if disp.x * disp.y == 0: disp/len(disp) else: disp/abs(disp))
 
 let mdir = {'L': vec(-1, 0), 'R': vec(1, 0), 'U': vec(0, -1), 'D': vec(0, 1)}.toTable
-let moves = fil(9).mapIt(it.scanTuple("$c $i")).filterIt(it[0]).mapIt(mdir[it[1]] * it[2])
 var knots = newSeq[Vec2i](10)
 var trail1, trail9: HashSet[(int, int)]
-for m in moves:
+for m in fil(9).mapIt(it.scanTuple("$c $i")).filterIt(it[0]).mapIt(mdir[it[1]] * it[2]):
     let dir = m/len(m)
     for step in 0..<m.len:
         knots[0] += dir
--- aoc.nim
+++ aoc.nim
@@ -39,3 +39,19 @@ type
 iterator slidingWindow*(dt: string, size: int): WindowView =
     for i in 0..dt.len-size:
         yield WindowView(index: i, view: dt[i..<i+size])
+
+
+type
+    Vec2i = object
+        x, y: int
+
+proc vec(x, y: int): Vec2i = Vec2i(x: x, y: y)
+proc `*`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x*b, y: a.y*b)
+proc `/`(a: Vec2i, b: Vec2i): Vec2i = Vec2i(x: a.x div b.x, y: a.y div b.y)
+proc `/`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x div b, y: a.y div b)
+proc `-`(a, b: Vec2i): Vec2i = Vec2i(x: a.x - b.x, y: a.y - b.y)
+proc `+`(a, b: Vec2i): Vec2i = Vec2i(x: a.x + b.x, y: a.y + b.y)
+proc `+=`(a: var Vec2i, b: Vec2i) = a = a+b
+proc abs(a: Vec2i): Vec2i = Vec2i(x: abs(a.x), y: abs(a.y))
+proc len(a: Vec2i): int = max(abs(a.x), abs(a.y))
+proc asTuple(v: Vec2i): (int, int) = (v.x, v.y)
