bc490faa4fb9cb13cd4fe849ef924380fdea7e18
Tommi Hyppänen <thyppane@gmail.com>
2022-12-09 16:07:55 +0200

    37 lines

--- 9/shorter.nim
+++ 9/shorter.nim
@@ -1,8 +1,7 @@
 include ../aoc
-import std/[options]
 
 type
-    Vec2i = object 
+    Vec2i = object
         x, y: int
 
 proc vec(x, y: int): Vec2i = Vec2i(x: x, y: y)
@@ -11,19 +10,10 @@ proc `/`(a: Vec2i, b: Vec2i): Vec2i = Vec2i(x: a.x div b.x, y: a.y div b.y)
 proc `/`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x div b, y: a.y div b)
 proc `-`(a, b: Vec2i): Vec2i = Vec2i(x: a.x - b.x, y: a.y - b.y)
 proc `+`(a, b: Vec2i): Vec2i = Vec2i(x: a.x + b.x, y: a.y + b.y)
-proc `+=`(a: var Vec2i, b: Vec2i) = a = a+b 
+proc `+=`(a: var Vec2i, b: Vec2i) = a = a+b
 proc abs(a: Vec2i): Vec2i = Vec2i(x: abs(a.x), y: abs(a.y))
 proc len(a: Vec2i): int = max(abs(a.x), abs(a.y))
 proc asTuple(v: Vec2i): (int, int) = (v.x, v.y)
-
-# (0, 0) is left up cause of how arrays are printed
-proc moveDir(c: char): Option[Vec2i] =
-    case c: 
-        of 'L': some(Vec2i(x: -1, y: 0))
-        of 'R': some(Vec2i(x: 1, y: 0))
-        of 'U': some(Vec2i(x: 0, y: -1))
-        of 'D': some(Vec2i(x: 0, y: 1))
-        else: none(Vec2i)
 
 proc follow(tail: var Vec2i, head: Vec2i) =
     let disp = head - tail
@@ -31,14 +21,10 @@ proc follow(tail: var Vec2i, head: Vec2i) =
         if disp.x * disp.y == 0: tail += disp/len(disp)
         else: tail += disp/abs(disp)
 
-let moves = fil(9).
-    mapIt(it.scanTuple("$c $i")).
-    filterIt(it[0]).
-    mapIt(it[1].moveDir.get * it[2])
-
+let mdir = {'L': vec(-1, 0), 'R': vec(1, 0), 'U': vec(0, -1), 'D': vec(0, 1)}.toTable
+let moves = fil(9).mapIt(it.scanTuple("$c $i")).filterIt(it[0]).mapIt(mdir[it[1]] * it[2])
 var knots = newSeq[Vec2i](10)
-var trail1: HashSet[(int, int)]
-var trail9: HashSet[(int, int)]
+var trail1, trail9: HashSet[(int, int)]
 for m in moves:
     let dir = m/len(m)
     for step in 0..<m.len:
@@ -48,5 +34,4 @@ for m in moves:
         trail1.incl(knots[1].asTuple)
         trail9.incl(knots[9].asTuple)
 
-echo trail1.len
-echo trail9.len
+echo fmt"1: {trail1.len}, 2: {trail9.len}"
