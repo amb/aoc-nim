34a44318f31e1c04163fa1bf956f86a5b3e0b835
Tommi Hyppänen <thyppane@gmail.com>
2022-12-09 15:39:39 +0200

    Vec2i

--- 9/shorter.nim
+++ 9/shorter.nim
@@ -2,33 +2,33 @@ include ../aoc
 import std/[options]
 
 type
-    Vec2D = object 
+    Vec2i = object 
         x, y: int
 
-proc vec(x, y: int): Vec2D = Vec2D(x: x, y: y)
-proc `*`(a: Vec2D, b: int): Vec2D = Vec2D(x: a.x*b, y: a.y*b)
-proc `/`(a: Vec2D, b: Vec2D): Vec2D = Vec2D(x: a.x div b.x, y: a.y div b.y)
-proc `/`(a: Vec2D, b: int): Vec2D = Vec2D(x: a.x div b, y: a.y div b)
-proc `-`(a, b: Vec2D): Vec2D = Vec2D(x: a.x - b.x, y: a.y - b.y)
-proc `+`(a, b: Vec2D): Vec2D = Vec2D(x: a.x + b.x, y: a.y + b.y)
-proc `+=`(a: var Vec2D, b: Vec2D) = a = a+b 
-proc abs(a: Vec2D): Vec2D = Vec2D(x: abs(a.x), y: abs(a.y))
-proc len(a: Vec2D): int = max(abs(a.x), abs(a.y))
-proc asTuple(v: Vec2D): (int, int) = (v.x, v.y)
-proc touching(a, b: Vec2D): bool =
+proc vec(x, y: int): Vec2i = Vec2i(x: x, y: y)
+proc `*`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x*b, y: a.y*b)
+proc `/`(a: Vec2i, b: Vec2i): Vec2i = Vec2i(x: a.x div b.x, y: a.y div b.y)
+proc `/`(a: Vec2i, b: int): Vec2i = Vec2i(x: a.x div b, y: a.y div b)
+proc `-`(a, b: Vec2i): Vec2i = Vec2i(x: a.x - b.x, y: a.y - b.y)
+proc `+`(a, b: Vec2i): Vec2i = Vec2i(x: a.x + b.x, y: a.y + b.y)
+proc `+=`(a: var Vec2i, b: Vec2i) = a = a+b 
+proc abs(a: Vec2i): Vec2i = Vec2i(x: abs(a.x), y: abs(a.y))
+proc len(a: Vec2i): int = max(abs(a.x), abs(a.y))
+proc asTuple(v: Vec2i): (int, int) = (v.x, v.y)
+proc touching(a, b: Vec2i): bool =
     let adisp = (a-b).abs
     adisp.x <= 1 and adisp.y <= 1
 
 # (0, 0) is left up cause of how arrays are printed
-proc moveDir(c: char): Option[Vec2D] =
+proc moveDir(c: char): Option[Vec2i] =
     case c: 
-        of 'L': some(Vec2D(x: -1, y: 0))
-        of 'R': some(Vec2D(x: 1, y: 0))
-        of 'U': some(Vec2D(x: 0, y: -1))
-        of 'D': some(Vec2D(x: 0, y: 1))
-        else: none(Vec2D)
+        of 'L': some(Vec2i(x: -1, y: 0))
+        of 'R': some(Vec2i(x: 1, y: 0))
+        of 'U': some(Vec2i(x: 0, y: -1))
+        of 'D': some(Vec2i(x: 0, y: 1))
+        else: none(Vec2i)
 
-proc follow(tail: var Vec2D, head: Vec2D) =
+proc follow(tail: var Vec2i, head: Vec2i) =
     let disp = head - tail
     if tail.touching(head): return
     if disp == vec(-2, 0): tail += vec(-1, 0)
@@ -42,7 +42,7 @@ let moves = fil(9).
     filterIt(it[0]).
     mapIt(it[1].moveDir.get * it[2])
 
-var knots = newSeq[Vec2D](10)
+var knots = newSeq[Vec2i](10)
 var trail1: HashSet[(int, int)]
 var trail9: HashSet[(int, int)]
 for m in moves:
