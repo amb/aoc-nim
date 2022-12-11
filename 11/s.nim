122bb68c53a8232a30f9c0bc63a377630a77aaa2
Tommi Hyppänen <thyppane@gmail.com>
2022-12-11 15:38:40 +0200

    11.2

--- 11/s.nim
+++ 11/s.nim
@@ -1,6 +1,6 @@
 include ../aoc
 
-type 
+type
     OpType = object
         value: int
         isSelf: bool
@@ -8,7 +8,8 @@ type
     Monkey = ref object
         inspections: int
         items: seq[int]
-        tests: array[3, int]
+        divider: int
+        targets: array[2, int]
         op: OpType
 
 let monkeys = collect:
@@ -16,34 +17,38 @@ let monkeys = collect:
         var newMonkey = Monkey()
         for lc, line in monkeyIn.splitLines().toSeq.pairs:
             let parts = line.split(":")
-            if lc == 1: newMonkey.items = collect(for n in parts[1].strip.split(", "): n.parseInt)
-            if lc >= 3 and lc <= 5: newMonkey.tests[lc-3] = parts[1].split(" ")[^1].parseInt
+            if lc == 1:
+                newMonkey.items = collect:
+                    for n in parts[1].strip.split(", "):
+                        n.parseInt
             if lc == 2:
                 let ip = parts[1].split(" ")[^2..^1]
                 newMonkey.op.op = ip[0][0]
                 newMonkey.op.isSelf = not intParser(ip[1], newMonkey.op.value)
+            if lc == 3:
+                newMonkey.divider = parts[1].split(" ")[^1].parseInt
+            if lc == 4 or lc == 5:
+                newMonkey.targets[lc-4] = parts[1].split(" ")[^1].parseInt
         newMonkey
 
-# for mnk in monkeys:
-#     echo mnk.items
-#     echo mnk.tests
-#     echo mnk.op.op, " ", mnk.op.value, " ", mnk.op.isSelf
+let commonDivider = monkeys.mapIt(it.divider).foldl(a*b)
 
-for round in 1..20:
+for round in 1..10000:
     for mi in 0..<monkeys.len:
-        # echo "--- ", mi
         let mnk = monkeys[mi]
         for i in mnk.items:
             inc mnk.inspections
-            var worry = 0
-            worry += i
+            var worry = i
             let wval = (if mnk.op.isSelf: worry else: mnk.op.value)
             if mnk.op.op=='+': worry += wval else: worry *= wval
-            # echo worry
-            worry = worry div 3
-            # echo worry
-            let pick = int(worry mod mnk.tests[0] != 0)
-            monkeys[mnk.tests[pick+1]].items.add(worry)
+            # worry = worry div 3
+            let pick = ((worry mod mnk.divider) != 0).int
+            monkeys[mnk.targets[pick]].items.add(worry mod commonDivider)
         mnk.items.setLen(0)
+
+    if round in [20, 1000, 5000, 10000]:
+        echo "Round: ", round
+        for mnk in monkeys:
+            echo mnk.inspections
 
 echo monkeys.mapIt(it.inspections).sorted[^2..^1].foldl(a*b)
