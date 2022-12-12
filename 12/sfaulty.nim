4c693cf1bd106ad2b75f35718cb48d1726af1e18
Tommi Hyppänen <thyppane@gmail.com>
2022-12-12 21:04:52 +0200

    Working part 1 thanks to frankzig

--- 12/sfaulty.nim
+++ 12/sfaulty.nim
@@ -1,6 +1,15 @@
 include ../aoc
 
-let data = "12/input".readFile.strip.splitLines
+let test = """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+"""
+
+let data = readFile("12/input").strip.splitLines
+# let data = test.strip.splitLines
 
 # grid[rows][columns]
 var startLoc, endLoc: (int, int)
@@ -8,24 +17,23 @@ var grid = collect:
     for y, line in data:
         collect:
             for x, c in line:
-                if c=='S': startLoc = (y, x)
-                if c=='E': endLoc = (y, x)
-                c
+                if c-'a' == -14: startLoc = (y, x)
+                if c-'a' == -28: endLoc = (y, x)
+                c-'a'
 
 let height = grid.len
 let width = grid[0].len
 echo fmt"width: {width}, height: {height}"
 echo fmt"start: {startLoc}, end: {endLoc}"
-grid[endLoc] = 'z'
-grid[startLoc] = 'a'
+grid[endLoc] = 'z'-'a'+1
 
 proc neighbours(l: (int, int)): seq[(int, int)] =
     let ly= l[0]
     let lx = l[1]
     @[(ly-1, lx), (ly+1, lx), (ly, lx-1), (ly, lx+1)]
 
-proc directions(grd: seq[seq[char]], loc: (int, int),
-    test: proc (a: char, b: char): bool): seq[(int, int)] =
+proc directions(grd: seq[seq[int]], loc: (int, int),
+    test: proc (a: int, b: int): bool): seq[(int, int)] =
     let grw = grd[0].len
     let grh = grd.len
     collect:
@@ -35,20 +43,28 @@ proc directions(grd: seq[seq[char]], loc: (int, int),
                     (y, x)
 
 # Dijkstra wavefront
-var previous = {startLoc: startLoc}.toTable
-var waveFront = [startLoc].toHashSet
-var newFront: HashSet[(int, int)]
+grid[startLoc] = 0
+var previous = newSeqWith(height, newSeq[(int, int)](width))
+for y in 0..<height:
+    for x in 0..<width:
+        previous[y][x] = (-1, -1)
 var step = 0
+var traveled = {startLoc: 0}.toTable
+var newFront: HashSet[(int, int)]
+var waveFront = [startLoc].toHashSet
 while true:
     for item in waveFront:
-        for loc in grid.directions(item, (a, b) => (a.ord >= b.ord - 1)):
-            if loc notin previous:
+        for loc in grid.directions(item, (a, b) => (a >= b - 1)):
+            if loc notin traveled:
                 previous[loc] = item
                 newFront.incl(loc)
+                traveled[loc] = step
     if newFront.len == 0 or endLoc in newFront: break
     waveFront = newFront
     newFront.clear
     inc step
+    # if step == 100:
+    #     break
 
 # Track back
 var pathBack: seq[(int, int)]
@@ -58,8 +74,16 @@ while head != startLoc:
     pathBack.add(head)
 
 # Print path back and map
-var drawTv = deepCopy grid
+var drawTv = newSeqWith(height, newSeq[char](width))
+for y in 0..<height:
+    for x in 0..<width:
+        drawTv[y][x] = 'a' + grid[y][x]
+
 for t in pathBack: drawTv[t] = ' '
+
+# for (k, v) in traveled.pairs:
+#     # echo k, " ", v
+#     drawTv[k] = '#'
 
 echo collect(for y in 0..<height: 
         collect(for x in 0..<width: 
@@ -69,5 +93,3 @@ assert pathBack.len == pathBack.deduplicate.len
 echo pathBack.len
 
 #not 498, not 499, not 500
-
-
