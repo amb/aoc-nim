8bc6b32e472505aaa202dab4a704fe06d612a0b0
Tommi Hyppänen <thyppane@gmail.com>
2022-12-12 19:08:44 +0200

    Non-working

--- 12/s.nim
+++ 12/s.nim
@@ -26,6 +26,8 @@ let width = grid[0].len
 echo fmt"width: {width}, height: {height}"
 echo fmt"start: {startLoc}, end: {endLoc}"
 
+grid[endLoc] = 'z'-'a'+1
+
 proc printGrid(gd: seq[seq[int]]): string =
     collect(for y in 0..<gd.len:
         let row = collect(for x in 0..<gd[0].len: 'a'+gd[y][x]).join
@@ -35,7 +37,7 @@ var waveFront = [startLoc].toHashSet
 var distanceGrid = newSeqWith(height, newSeq[int](width))
 for y in 0..<height:
     for x in 0..<width:
-        distanceGrid[y][x] = int.high
+        distanceGrid[y][x] = int.high div 2
 
 proc inGrid(loc: (int, int), grd: seq[seq[int]]): bool =
     let x = loc[1]
@@ -65,15 +67,13 @@ proc whereCanGo(grd: seq[seq[int]], loc: (int, int),
 # Dijkstra wavefront
 var step = 0
 var traveled = [startLoc].toHashSet
-grid[endLoc] = int.high
 grid[startLoc] = 0
 while true:
     var newFront: HashSet[(int, int)]
     for item in waveFront:
-        distanceGrid[item] = step
-        # -28 = endloc value
-        for (y, x) in grid.whereCanGo(item, (a, b) => (a == b - 1 or a == b or b == -28)):
+        for (y, x) in grid.whereCanGo(item, (a, b) => (a >= b - 1)):
             if (y, x) notin traveled:
+                distanceGrid[item] = step
                 newFront.incl((y, x))
                 traveled.incl((y, x))
     if newFront.len == 0: break
@@ -84,14 +84,13 @@ while true:
 echo "Steps: ", step
 doAssert step > 0
 
+echo distanceGrid[endLoc]
+echo distanceGrid[endLoc[0]][endLoc[1]+1]
+
 var drawTv = newSeqWith(height, newSeq[char](width))
 for y in 0..<height:
     for x in 0..<width:
-        drawTv[y][x] = '.'
-for t in traveled:
-    drawTv[t] = '#'
-echo collect(for y in 0..<height: collect(for x in 0..<width: drawTv[y][x]).join & "\n").join
-
+        drawTv[y][x] = 'a'+grid[y][x]
 
 # Track back
 var pathBack = @[endLoc]
@@ -99,15 +98,37 @@ distanceGrid[endLoc] = collect(
     for l in endLoc.neighbours.filterIt(it.inGrid(grid)): 
         distanceGrid[l]).max + 1
 
-while distanceGrid[pathBack[^1]] > 0:
+echo grid[endLoc]
+echo grid[endLoc[0]][endLoc[1]+1]
+echo distanceGrid[endLoc]
+
+while true:
     var loc = pathBack[^1]
     var value = distanceGrid[loc]
     let lookHere = loc.neighbours.filterIt(it.inGrid(grid))
-    let floc = lookHere.findFirst(x => distanceGrid[x] == value - 1)
+    # Track back one step with max 1 unit drop
+    let floc = lookHere.findFirst(x => distanceGrid[x]==value-1 and grid[loc]-grid[x]<=1)
     if not floc.isSome:
         echo "broke"
         break
     pathBack.add(floc.get)
 
-echo pathBack
-echo pathBack.len - 1
+for ti, t in pathBack:
+    drawTv[t] = 'a'+grid[t]
+    if ti > 0:
+        let dif = vec2i(pathBack[ti-1])-vec2i(t)
+        if dif == vec2i(-1, 0): drawTv[t] = '^'
+        if dif == vec2i(1, 0): drawTv[t] = 'v'
+        if dif == vec2i(0, -1): drawTv[t] = '<'
+        if dif == vec2i(0, 1): drawTv[t] = '>'
+        drawTv[t] = ' '
+
+echo collect(for y in 0..<height: 
+        collect(for x in 0..<width: 
+            drawTv[y][x]
+        ).join & "\n"
+    ).join
+
+# echo pathBack[0..3]
+assert pathBack.len == pathBack.deduplicate.len
+echo pathBack.len-1
