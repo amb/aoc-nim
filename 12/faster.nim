05da39a9019cf2fc507226d4e13c2f889ac9b643
Tommi Hyppänen <thyppane@gmail.com>
2022-12-13 08:49:33 +0200

    Day 13

--- 12/faster.nim
+++ 12/faster.nim
@@ -44,16 +44,37 @@ proc forwardWavefront(grd: seq[char], mask: var seq[int], sLoc: int, eLoc: int):
             waveFront.add(it)
     step
 
+iterator backwardsWavefront0(grd: seq[char], mask: var seq[int], sLoc: int): (int, int) =
+    var waveFront = @[sLoc]
+    var newFront: seq[int]
+    var step = 0
+    while waveFront.len > 0:
+        inc step
+        newFront.setLen(0)
+        for item in waveFront:
+            if item mod width == 0:
+                yield ((item mod width), step)
+            mask[item] = 1
+            for d in dirs:
+                let loc = item + d
+                if loc >= 0 and loc < asize and 
+                grd[item].ord - grd[loc].ord < 1 and mask[loc] != 1:
+                    newFront.add(loc)
+                    mask[loc] = 1
+        waveFront.setLen(0)
+        for it in newFront:
+            waveFront.add(it)
+
 var timeStart = getMonoTime()
 
 var minVal = int.high
-for i in 0..<1:
+for i in 0..<height:
     for s in 0..<asize: mask[s] = 0
     minVal=min(minVal, forwardWavefront(grid, mask, i*width, endLoc))
+echo minVal
+# echo backwardsWavefront0(grid, mask, endLoc).toSeq
 
 let mstime = (getMonoTime() - timeStart).inMicroseconds
 let mlsecs = mstime div 1000
 let mcsecs = mstime - (mlsecs * 1000)
 echo "Time: ", mlsecs," ms, ", mcsecs, " us"
-
-echo minVal
