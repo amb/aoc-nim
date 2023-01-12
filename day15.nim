3eb04700a91a207246a4d9c4e33e2e3d7b16ab52
Tommi Hyppänen <thyppane@gmail.com>
2023-01-12 15:05:48 +0200

    Faster day 15

--- day15.nim
+++ day15.nim
@@ -1,12 +1,12 @@
 include aoc
-import std/[parseutils]
+import std/parseutils
 import shadows
 
 day 15:
     type
         Sensor = object
             location: Coord2D
-            area: int64
+            area: int
 
     proc sensorScanline(sensor: Sensor, y: int64): Option[(int64, int64)] =
         let ydisp = abs(sensor.location.y-y)
@@ -14,19 +14,6 @@ day 15:
             return none((int64, int64))
         let closeToFringe = (sensor.area-ydisp)
         return some((sensor.location.x - closeToFringe, sensor.location.x + closeToFringe))
-
-    proc findBacon(sensors: seq[Sensor], lval, hval: int64): (int64, int64) =
-        var slines: ShadowLines
-        for ry in lval..hval:
-            for si, s in sensors:
-                let r = s.sensorScanline(ry)
-                if r.isSome:
-                    slines.addShadow(r.get)
-            slines.finalize()
-            for e in slines.empties(lval, hval):
-                if e[1]-e[0] > 1:
-                    return (e[0]+1, ry)
-            slines.reset()
 
     proc scanLine(sensors: seq[Sensor], lval: int64): int64 =
         var slines: ShadowLines
@@ -41,13 +28,47 @@ day 15:
 
     const dscan = "Sensor at x=$i, y=$i: closest beacon is at x=$i, y=$i"
     var sensors: seq[Sensor]
-    for line in lines:
+    for line in "15/input".lines:
         var sx, sy, bx, by: int
         if line.scanf(dscan, sx, sy, bx, by):
             let s = coord2d(sx, sy)
             sensors.add(Sensor(location: s, area: s.manhattan(coord2d(bx, by))))
 
     part 1, 4725496: sensors.scanLine(2000000) - 1
+
+    # 1. an isolated cell can only appear in the gap between two parallel lines with distance 2 between
+    # 2. in fact 2 sets of parallel lines, one pair in the / direction and one pair in the \ direction
+    # 3. so if you find such an arrangement in the / and \ direction, their intersections gives you the only possible candidates for such a location
+    # 4. this solution only scales with the number of lines of input
+
+    proc atZero(o: Coord2D, v: Coord2D, area, s: int): int =
+        # s=-1 => /, s=1  => \
+        o.y + (o.x + area * v.sgn.x + v.sgn.x) * s
+
+    # Get all sensors where there's 1 space in between
+    var y0, y1: int64
+    for y in 0..<sensors.len:
+        for x in y+1..<sensors.len:
+            let sy = sensors[y]
+            let sx = sensors[x]
+            if sy.location.manhattan(sx.location) == sy.area + sx.area + 2:
+                # From sx to sy
+                let diff = sy.location - sx.location
+                let ds = diff.sgn
+                
+                # \ line
+                if ds in [coord2d(1, 1), coord2d(-1, -1)]:
+                    y0 = atZero(sx.location, diff, sx.area, 1)
+
+                # / line
+                if ds in [coord2d(-1, 1), coord2d(1, -1)]:
+                    y1 = atZero(sx.location, diff, sx.area, -1)
+
+    # Line equation
+    # y1 - x == y2 + x
+    # y1-y2 = 2x
+    # x = (y1-y2)/2
+
     part 2, 12051287042458.int64:
-        let locs = sensors.findBacon(0, 4000000)
-        locs[0]*4000000+locs[1]
+        let x = (y0-y1) div 2
+        x*4000000+y1+x
