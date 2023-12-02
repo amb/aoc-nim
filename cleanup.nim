2e0859bfb7262d63a9429d72bbbedc9507140b31
Tommi Hyppänen <thyppane@gmail.com>
2023-12-02 05:15:49 +0200

    Other OS cleanup

--- cleanup.nim
+++ cleanup.nim
@@ -2,7 +2,7 @@ import std/[os, strutils, sequtils, sugar]
 
 let destroy = collect:
     for d in walkDirRec("."):
-        if "cleanup" notin d and ".git" notin d and ".exe" in d:
+        if "cleanup" notin d and ".git" notin d:
             d
 
 for f in destroy:
