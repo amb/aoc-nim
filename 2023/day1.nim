4c076fe772fad2ecd2a78639ce6d6e5aad4a6206
Tommi Hyppänen <thyppane@gmail.com>
2023-12-02 17:25:11 +0200

    Faster day1

--- 2023/day1.nim
+++ 2023/day1.nim
@@ -1,41 +1,48 @@
 import ../aoc
-import strutils, tables
+import strutils, tables, strformat
 
+func toInt(c: char): int = int(c) - int('0')
 
-func toInt(c: char): int =
-    return int(c) - int('0')
+type AccParse = object
+    names: seq[string]
+    locs: seq[int]
 
-const tnums = @{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-    "six": 6, "seven": 7, "eight": 8, "nine": 9}.toTable
+proc newAccParse(names: seq[string]): AccParse =
+    result = AccParse(names: names, locs: newSeq[int](names.len))
 
-# TODO: parser that tracks location in each word (include also numbers, len: 1)
-#       for both back and forward search. Have to find only first from beginning and end
+proc parse(ap: var AccParse, c: char): int =
+    result = -1
+    for ni, n in ap.names:
+        if n[ap.locs[ni]] == c:
+            ap.locs[ni] = ap.locs[ni] + 1
+        else:
+            # ffive
+            ap.locs[ni] = (if n[0] != c: 0 else: 1)
 
-proc endsWithNumber(i: seq[char]): int =
-    for k in tnums.keys:
-        if i.len < k.len:
-            continue
-        for n in 1..k.len:
-            if i[^n] != k[^n]:
-                break
-            if n == k.len:
-                return tnums[k]
-    return 0
+        if ap.locs[ni] == n.len:
+            result = ni
+            break
 
-proc funkyDigits(i: string): seq[int] =
-    var accumulator: seq[char] = @[]
-    for n in 0..<i.len:
-        let c = i[n]
+proc parseReverse(ap: var AccParse, c: char): int =
+    result = -1
+    for ni, n in ap.names:
+        if n[ap.locs[ni]] == c:
+            ap.locs[ni] = ap.locs[ni] - 1
+        else:
+            ap.locs[ni] = (if n[n.len - 1] != c: n.len - 1 else: n.len - 2)
 
-        if isDigit(c):
-            result.add(c.toInt)
-            accumulator.setLen(0)
-            continue
+        if ap.locs[ni] == -1:
+            result = ni
+            break
 
-        accumulator.add(c)
-        let ewn = endsWithNumber(accumulator)
-        if ewn != 0:
-            result.add(ewn)
+proc reset(ap: var AccParse) =
+    for ni in 0..<ap.names.len:
+        ap.locs[ni] = 0
+
+proc resetReverse(ap: var AccParse) =
+    for ni in 0..<ap.names.len:
+        ap.locs[ni] = ap.names[ni].len - 1
+
 
 day 1:
     part 1, 54338:
@@ -53,8 +60,26 @@ day 1:
         total
 
     part 2, 53389:
+        func asInt(s: int): int = (if s < 9: s + 1 else: s - 8)
+        const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
+                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+        var pac = newAccParse(@numbers)
+
         var total = 0
         for line in lines:
-            let nums = line.funkyDigits
-            total += nums[0] * 10 + nums[^1]
+            pac.resetReverse
+            for ci in countdown(line.high, line.low):
+                let c = line[ci]
+                let r = pac.parseReverse(c)
+                if r >= 0:
+                    total += r.asInt()
+                    break
+
+            pac.reset
+            for ci, c in line:
+                let r = pac.parse(c)
+                if r >= 0:
+                    total += r.asInt() * 10
+                    break
         total
