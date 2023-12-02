7cb49b352ec8422bdf757c4371fee8e88888c4b2
Tommi Hyppänen <thyppane@gmail.com>
2023-12-02 02:11:29 +0200

    Faster

--- 2023/day1.nim
+++ 2023/day1.nim
@@ -9,6 +9,17 @@ func digits(i: string): seq[char] =
 let tnums = @{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
     "six": 6, "seven": 7, "eight": 8, "nine": 9}.toTable
 
+proc endsWithNumber(i: seq[char]): int =
+    for k in tnums.keys:
+        if i.len < k.len:
+            continue
+        for n in 1..k.len:
+            if i[^n] != k[^n]:
+                break
+            if n == k.len:
+                return tnums[k]
+    return 0
+
 proc funkyDigits(i: string): seq[string] =
     var accumulator: seq[char] = @[]
     for n in 0..<i.len:
@@ -20,13 +31,9 @@ proc funkyDigits(i: string): seq[string] =
             continue
         
         accumulator.add(c)
-        let current = accumulator.join("")
-        for k, v in tnums:
-            if current.endsWith(k):
-                result.add($v)
-                # accumulator.setLen(0)
-                break
-
+        let ewn = endsWithNumber(accumulator)
+        if ewn != 0:
+            result.add($ewn)
 
 day 1:
     part 1, 54338:
