62966222d21b832d3d0f738d94b927be164a9186
Tommi Hyppänen <thyppane@gmail.com>
2023-12-02 02:37:35 +0200

    Faster+

--- 2023/day1.nim
+++ 2023/day1.nim
@@ -1,10 +1,14 @@
 import ../aoc
 import sugar, strutils, math, tables
 
-func digits(i: string): seq[char] =
+
+func toInt(c: char): int =
+    return int(c) - int('0')
+
+func digits(i: string): seq[int] =
     for c in i:
         if isDigit(c):
-            result.add(c)
+            result.add(c.toInt)
 
 let tnums = @{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
     "six": 6, "seven": 7, "eight": 8, "nine": 9}.toTable
@@ -20,32 +24,32 @@ proc endsWithNumber(i: seq[char]): int =
                 return tnums[k]
     return 0
 
-proc funkyDigits(i: string): seq[string] =
+proc funkyDigits(i: string): seq[int] =
     var accumulator: seq[char] = @[]
     for n in 0..<i.len:
         let c = i[n]
 
         if isDigit(c):
-            result.add($c)
+            result.add(c.toInt)
             accumulator.setLen(0)
             continue
-        
+
         accumulator.add(c)
         let ewn = endsWithNumber(accumulator)
         if ewn != 0:
-            result.add($ewn)
+            result.add(ewn)
 
 day 1:
     part 1, 54338:
-        let truenums = collect:
-            for line in lines:
-                let nums = line.digits
-                parseInt($nums[0] & $nums[^1])
-        sum(truenums)
+        var total = 0
+        for line in lines:
+            let nums = line.digits
+            total += nums[0] * 10 + nums[^1]
+        total
 
     part 2, 53389:
-        let truenums = collect:
-            for line in lines:
-                let nums = line.funkyDigits
-                parseInt($nums[0] & $nums[^1])
-        sum(truenums)
+        var total = 0
+        for line in lines:
+            let nums = line.funkyDigits
+            total += nums[0] * 10 + nums[^1]
+        total
