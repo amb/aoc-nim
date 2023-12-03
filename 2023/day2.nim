aae6f4eb86eb1a6ebae174905a1a3fe74adf7a92
Tommi Hyppänen <thyppane@gmail.com>
2023-12-03 18:52:33 +0200

    fix

--- 2023/day2.nim
+++ 2023/day2.nim
@@ -2,41 +2,42 @@ import ../aoc
 import strutils, math, sequtils, npeg, tables
 
 day 2:
-    # let lines = input.splitLines
+    let lines = input.splitLines
 
     type Colors = array[3, int]
     const cname = {"red": 0, "green": 1, "blue": 2}.toTable
-
-    # for line in lines:
-    #     let game_sets = line.split(": ")[1].split("; ")
-    #     var color_sets = newSeq[Colors]()
-    #     for fistful in game_sets:
-    #         var tcol = [0, 0, 0]
-    #         for color in fistful.split(", "):
-    #             let color = color.split(" ")
-    #             tcol[cname[color[1]]] = color[0].parseInt()
-    #         color_sets.add(tcol)
-    #     games.add(color_sets)
+    
+    var games: seq[seq[Colors]]
+    for line in lines:
+        let game_sets = line.split(": ")[1].split("; ")
+        var color_sets = newSeq[Colors]()
+        for fistful in game_sets:
+            var tcol = [0, 0, 0]
+            for color in fistful.split(", "):
+                let color = color.split(" ")
+                tcol[cname[color[1]]] = color[0].parseInt()
+            color_sets.add(tcol)
+        games.add(color_sets)
 
     # use npeg to parse lines
     # games[seq] -> colorseq[seq] -> r, g, b
-    let parser = peg("root", games: seq[seq[Colors]]):
-        root <- +(game * '\n')
-        game <- *(colorset * "; ") * colorset:
-            games.add(newSeq[Colors]())
-        colorset <- *(color * ", ") * color:
-            games[^1].add([0, 0, 0])
-        color <- >(+Digit) * ' ' * >(+Alpha):
-            games[^1][^1][cname[$2]] = parseInt($1)
+    # let parser = peg("root", games: seq[seq[Colors]]):
+    #     root <- +(game * '\n')
+    #     game <- *(colorset * "; ") * colorset:
+    #         games.add(newSeq[Colors]())
+    #     colorset <- *(color * ", ") * color:
+    #         games[^1].add([0, 0, 0])
+    #     color <- >(+Digit) * ' ' * >(+Alpha):
+    #         games[^1][^1][cname[$2]] = parseInt($1)
     
-    var g: seq[seq[Colors]]
-    echo "Parsing result: ", parser.match(input, g).ok
-    echo g.len
+    # var g: seq[seq[Colors]]
+    # echo "Parsing result: ", parser.match(input, g).ok
+    # echo g.len
 
     part 1, 2006: 
         let maxCubes = [12, 13, 14]
         var ids_sum = 0
-        for i, c in g:
+        for i, c in games:
             block outerloop:
                 for gc in c:
                     if gc[0] > maxCubes[0] or gc[1] > maxCubes[1] or gc[2] > maxCubes[2]:
@@ -46,7 +47,7 @@ day 2:
 
     part 2, 84911:
         var max_sets = newSeq[Colors]()
-        for i, c in g:
+        for i, c in games:
             var max_set = c[0]
             for gc in c:
                 for n in 0..2:
