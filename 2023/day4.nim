e46e28b6b239a8bb6949c6c5eb9ddc8705902b32
Tommi Hyppänen <thyppane@gmail.com>
2023-12-04 14:39:50 +0200

    cleanup

--- 2023/day4.nim
+++ 2023/day4.nim
@@ -1,13 +1,13 @@
 import ../aoc
-import std/[sequtils, strutils, sets, tables, math]
+import std/[sequtils, strutils, sets, tables, math, intsets]
 
 day 4:
     let lines = input.splitLines
 
     type Scratch = object
         index: int
-        winning: HashSet[int]
-        own: HashSet[int]
+        winning: IntSet
+        own: IntSet
         cardValue: int
         matching: int
         amount: int
@@ -15,30 +15,18 @@ day 4:
     var cards = newSeq[Scratch](lines.len)
 
     proc calcValue(c: var Scratch) =
-        var cval = 0
-        var mat = 0
-        for n in c.winning:
-            if n in c.own:
-                inc mat
-                if cval == 0:
-                    cval = 1
-                else:
-                    cval *= 2
-        c.cardValue = cval
+        let mat = (c.winning * c.own).len
+        c.cardValue = (if mat > 0: 2^(mat-1) else: 0)
         c.matching = mat
 
     var total1 = 0
     for index, l in lines:
         let nums = ints(l)
-        cards[index] = Scratch(index: nums[0], winning: nums[1..10].toSet, own: nums[11..^1].toSet, amount: 1)
+        cards[index] = Scratch(index: nums[0], winning: nums[1..10].toIntSet, own: nums[11..^1].toIntSet, amount: 1)
         cards[index].calcValue()
         total1 += cards[index].cardValue
 
-    assert cards[1].index == 2
-
-    part 1, 15205:
-        total1
-
+    part 1, 15205: total1
     part 2, 6189740:
         for idx, c in cards:
             var endPoint = idx + c.matching
@@ -46,5 +34,4 @@ day 4:
                 endPoint = cards.high
             for i in idx+1..endPoint:
                 cards[i].amount += c.amount
-            
         cards.mapIt(it.amount).sum
