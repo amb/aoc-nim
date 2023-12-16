8dd193cb9f7492d4991997c710de9a8cc2f6c063
Tommi Hyppänen <thyppane@gmail.com>
2023-12-16 04:19:23 +0200

    Day 6

--- 2023/day6.nim
+++ 2023/day6.nim
@@ -1,11 +1,47 @@
 import ../aoc
+import ../aoc_lib
 import std/[sequtils, strutils]
+
+const testInput = """Time:      7  15   30
+Distance:  9  40  200"""
 
 day 6:
     let lines = input.splitLines
+    # let lines = testInput.splitLines
+    let times = lines[0].parseUints64
+    let distances = lines[1].parseUints64
 
-    part 1:
-        404
+    part 1, 393120:
+        var totals = 1
+        for games in 0..<times.len:
+            let time = times[games]
+            let distance = distances[games]
+            var recordBeat = 0
+            for pressed in 0..time:
+                let speed = pressed
+                let timeRemainder = time - pressed
+                let distanceTraveled = speed * timeRemainder
+                if distanceTraveled > distance:
+                    inc recordBeat
+            totals *= recordBeat
+
+        totals
 
     part 2:
-        404
+        # Another brute-force 😓
+
+        let time: int64 = 62737565
+        let distance: int64 = 644102312401023
+
+        var totals = 1
+        var recordBeat = 0
+        for pressed in 0..time:
+            let speed = pressed
+            let timeRemainder = time - pressed
+            let distanceTraveled = speed * timeRemainder
+            if distanceTraveled > distance:
+                inc recordBeat
+        totals *= recordBeat
+
+        totals
+
